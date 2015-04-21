8b4e1558f09937c555ab381ea6399a2c0758c23d
Sebastian Thiel <byronimo@gmail.com>
2015-04-21 11:41:47 +0200

    fix(rustup): (07560d233 2015-04-20) (built 2015-04-19)
    * use std::io consistently
    * adjust to improved `Command` API
    
    Related to https://github.com/Byron/google-apis-rs/issues/70

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-#![feature(old_io)]
 //! Use this library to open a path or URL using the program configured on the system.
 //!
 //! # Usage
@@ -19,30 +18,36 @@
 //! As an operating system program is used, chances are that the open operation fails.
 //! Therfore, you are advised to at least check the result with `.is_err()` and 
 //! behave accordingly, e.g. by letting the user know what you tried to open, and failed.
-use std::old_io::IoResult;
-use std::old_io::process::{Command, ProcessExit};
+use std::io;
+use std::process::{Command, ExitStatus};
 
 #[cfg(not(any(target_os = "windows", target_os = "macos")))]
-pub fn that(path: &str) -> IoResult<ProcessExit> {
-    use std::old_io::IoError;
-
-    let mut res = Err(IoError::from_errno(0, false));
+pub fn that(path: &str) -> io::Result<ExitStatus> {
+    let mut last_err: io::Result<ExitStatus> = Err(io::Error::from_raw_os_error(0));
     for program in &["xdg-open", "gnome-open", "kde-open"] {
-        res = Command::new(program).arg(path).detached().status();
-        match res {
-            Ok(_) => return res,
-            Err(_) => continue,
+        match Command::new(program).arg(path).spawn() {
+            Ok(mut child) => return child.wait(),
+            Err(err) => {
+                last_err = Err(err);
+                continue;
+            },
         }
     }
-    res
+    last_err
 }
 
 #[cfg(target_os = "windows")]
-pub fn that(path: &str) -> IoResult<ProcessExit> {
-    Command::new("start").arg(path).detached().status()
+pub fn that(path: &str) -> io::Result<ExitStatus> {
+    match Command::new("start").arg(path).spawn() {
+        Ok(mut child) => child.wait(),
+        Err(err) => Err(err),
+    }
 }
 
 #[cfg(target_os = "macos")]
-pub fn that(path: &str) -> IoResult<ProcessExit> {
-    Command::new("open").arg(path).detached().status()
+pub fn that(path: &str) -> io::Result<ExitStatus> {
+    match Command::new("open").arg(path).spawn() {
+        Ok(mut child) => child.wait(),
+        Err(err) => Err(err),
+    }
 }

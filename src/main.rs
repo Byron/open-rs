25c0e398856c24a2daf0444640567ed3fd2f4307
Sebastian Thiel <byronimo@gmail.com>
2015-07-08 17:39:09 +0200

    fix(open): use result
    I wonder why that was not shown when I compiled it

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,13 @@ fn main() {
         match env::args().skip(1).next() {
             Some(arg) => arg,
             None => {
-                writeln!(stderr(), "usage: open <path-or-url>");
+                writeln!(stderr(), "usage: open <path-or-url>").ok();
                 process::exit(1);
             }
         };
 
     if let Err(err) = open::that(&path_or_url) {
-        writeln!(stderr(), "An error occourred when opening '{}': {}", path_or_url, err);
+        writeln!(stderr(), "An error occourred when opening '{}': {}", path_or_url, err).ok();
         process::exit(3);
     }
 }
